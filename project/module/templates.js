66aa40bcf91d0b0e9aa1fc26bca614da9a24d3b3
hzq <saoraozhe3hao@126.com>
2016-06-10 23:25:40 +0800

    更新

---
+++ project/module/templates.js
@@ -0,0 +1,10 @@
+;(function (templates, undefined) {
+  templates["project/module/unit/chart/chart.html"] = "<div id=\"chart\" class=\"chart\">\n" +
+    "    <canvas id=\"lineChart\" width=\"600\" height=\"400\"></canvas>\n" +
+    "    <button>操作</button>\n" +
+    "    <canvas id=\"barChart\" width=\"600\" height=\"400\"></canvas>\n" +
+    "    <canvas id=\"pieChart\" width=\"600\" height=\"400\"></canvas>\n" +
+    "    <canvas id=\"doughnutChart\" width=\"600\" height=\"400\"></canvas>\n" +
+    "</div>";
+  templates["project/module/unit/ztree/ztree.html"] = "<div id=\"ztree\" class=\"ztree\"></div>";
+})(this.templates = this.templates || {});
